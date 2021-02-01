611e4a85dd782d74a3d43ea3c6cf507d430ebdd2
taylnath <taylnath@oregonstate.edu>
2021-02-01 14:59:13 -0800

    handles input files

--- SeatTheater.py
+++ SeatTheater.py
@@ -7,10 +7,27 @@ class ExcessRowError(Exception):
     """
     pass
 
+
 class SeatTheater:
     """
     A class to determine an optimal seating arrangement in a theater. 
     """
+
+    def __init__(self, reservation_file, rows=10, cols=20, row_buffer=1, col_buffer=3):
+        """
+        Initialize a SeatTheater object with a given reservation_file,
+        number of rows (default 10), number of columns (default 20),
+        row buffer (default 1), and column buffer (default 3).
+        """
+
+        self._row_list = self.make_row_list(rows)
+        self._col_list = self.make_column_list(cols)
+
+        self._row_buffer = row_buffer
+        self._col_buffer = col_buffer
+
+        self._reservations = self.make_reservation_list(reservation_file)
+
 
     def make_row_list(self, rows):
         """
@@ -35,27 +52,42 @@ class SeatTheater:
 
         return [col + 1 for col in range(cols)]
 
-    def __init__(self, reservation_file, rows=10, cols=20, row_buffer=1, col_buffer=3):
+    def make_reservation_list(self, reservation_file):
         """
-        Initialize a SeatTheater object with a given reservation_file,
-        number of rows (default 10), number of columns (default 20),
-        row buffer (default 1), and column buffer (default 3).
+        Returns a list of tuples (reservation_name, number_of_guests)
+        of reservations parsed from the reservation_file. Assumes the 
+        reservation_file has the form "R123 8", where R123 is the reservation 
+        name and 8 is the number of guests.
         """
+
+        reservations = []
 
         try:
-            self._row_list = self.make_row_list(rows)
-            self._col_list = self.make_column_list(cols)
+            with open(reservation_file, 'r') as reservation_data:
+                for reservation_datum in reservation_data:
+                    reservation_name, text_number = reservation_datum.split()
+                    reservation_number = int(text_number)
+                    reservations.append((reservation_name, reservation_number))
+        except FileNotFoundError:
+            if __name__ == "__main__":
+                print("We could not find your reservation file.")
+            return []
+        except ValueError:
+            if __name__ == "__main__":
+                print("We could not parse part of your reservation file. Is it in the correct format?")
+            return []
         except:
-            print("We could not parse your theater row/column data. Did you provide more than 26 rows?")
-            return
+            if __name__ == "__main__":
+                print("Something went wrong. Please try again.")
+            return []
 
-        self._reservation_file = reservation_file
-        self._row_buffer = row_buffer
-        self._col_buffer = col_buffer
+        return reservations
 
 
 if __name__ == "__main__":
-    st = SeatTheater("hello")
+    st = SeatTheater("reservations.txt")
+
+    print(st._reservations)
 
     print(st._row_list)
     print(st._col_list)
--- TestSeatTheater.py
+++ TestSeatTheater.py
@@ -2,20 +2,42 @@
 # Last updated 2/1/2021
 
 import unittest
-from SeatTheater import SeatTheater
+from SeatTheater import SeatTheater, ExcessRowError
 
 class TestSeatTheater(unittest.TestCase):
     """
     Tests for the SeatTheater class.
     """
 
+    def setUp(self):
+        "Example test file"
+
+        self.test_file = "test_reservations.txt"
+        self.theater = SeatTheater(self.test_file)
+        self.bad_test_file = "bad_reservations.txt"
+
     def test_init(self):
         """
         Test initialization of the SeatTheater class.
         """
 
-        x = SeatTheater("testing")
-        self.assertEqual(x._row_list, ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'])
-        self.assertEqual(x._col_list, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+        # check row and column lists are ok
+        self.assertEqual(self.theater._row_list, ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'])
+        self.assertEqual(self.theater._col_list, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+
+        # test too many rows 
+        with self.assertRaises(ExcessRowError):
+            x = SeatTheater(self.test_file, rows=27)
+
+        # test file parsing errors
+        x = SeatTheater("not a file")
+        self.assertEqual(x._reservations, [])
+
+        x = SeatTheater(self.bad_test_file)
+        self.assertEqual(x._reservations, [])
+
+        # check that reservation list is ok
+        self.assertEqual(self.theater._reservations, [("R001", 2), ("R002", 4)])
+
 
 unittest.main()
